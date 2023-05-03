ec075aa9e9489ae97b93bc16591ab821a99d47a6
Adam Hutton <adamhutt717@gmail.com>
2023-05-03 17:18:42 +0100

    Add project files.

---
+++ BuildProjectManager/Menu.cs
@@ -0,0 +1,103 @@
+﻿
+using System.Text;
+
+namespace BuildProjectManager
+{
+    public static class ConsoleHelpers
+    {
+        public static int GetIntegerInRange(int pMin, int pMax, string pMessage)
+        {
+            if (pMin > pMax)
+            {
+                throw new Exception($"Minimum value {pMin} cannot be greater than maximum value {pMax}");
+            }
+
+            int result;
+
+            do
+            {
+                Console.WriteLine(pMessage);
+                Console.WriteLine($"Please enter a number between {pMin} and {pMax} inclusive.");
+
+                string userInput = Console.ReadLine();
+
+                try
+                {
+                    result = int.Parse(userInput);
+                }
+                catch
+                {
+                    Console.WriteLine($"{userInput} is not a number");
+                    continue;
+                }
+
+                if (result >= pMin && result <= pMax)
+                {
+                    return result;
+                }
+                Console.WriteLine($"{result} is not between {pMin} and {pMax} inclusive.");
+            } while (true);
+        }
+    }
+
+    // Base class for all menu items to inherit from
+    abstract class MenuItem
+    {
+        public abstract string MenuText();
+        public abstract void Select();
+    }
+
+    // Lays out basic functionality for menu items
+    abstract class ConsoleMenu : MenuItem
+    {
+        protected List<MenuItem> _menuItems = new List<MenuItem>();
+
+        public bool IsActive { get; set; }
+
+        public abstract void CreateMenu();
+
+        public override void Select()
+        {
+            IsActive = true;
+            do
+            {
+                CreateMenu(); // Re-initialises menu after returning from child menu item
+                string output = $"{MenuText()}{Environment.NewLine}";
+                int selection = ConsoleHelpers.GetIntegerInRange(1, _menuItems.Count, this.ToString()) - 1;
+                _menuItems[selection].Select();
+            } while (IsActive);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(MenuText());
+            for (int i = 0; i < _menuItems.Count; i++)
+            {
+                sb.AppendLine($"{i + 1}. {_menuItems[i].MenuText()}");
+            }
+            return sb.ToString();
+        }
+    }
+
+    // Returns to previous menu
+    class ReturnItem : MenuItem
+    {
+        private ConsoleMenu _menu;
+
+        public ReturnItem(ConsoleMenu parentItem)
+        {
+            _menu = parentItem;
+        }
+
+        public override string MenuText()
+        {
+            return "<-Return";
+        }
+
+        public override void Select()
+        {
+            _menu.IsActive = false; // Breaks out of do-while loop to return to the parent loop (Line 62)
+        }
+    }
+}
---
+++ BuildProjectManager/ProjectMenu.cs
@@ -0,0 +1,157 @@
+﻿
+using System.Text;
+
+namespace BuildProjectManager
+{
+    #region Entry menu and items
+
+    class EntryMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+            _menuItems.Clear();
+            //Populate list with menu items...
+        }
+
+        public override string MenuText()
+        {
+            return "Build Project Manager";
+        }
+    }
+
+    class LoadBeigeFileMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+
+        }
+
+        public override string MenuText()
+        {
+            return "Load Beige file from storage";
+        }
+    }
+
+    class DisplayPortfolioSummaryMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+
+        }
+
+        public override string MenuText()
+        {
+            return "Display a summary of entire portfolio transactions";
+        }
+    }
+
+    class AddProjectMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+
+        }
+
+        public override string MenuText()
+        {
+            return "Add new project";
+        }
+    }
+
+    class RemoveProjectMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+
+        }
+
+        public override string MenuText()
+        {
+            return "Remove existing project";
+        }
+    }
+
+    class SelectProjectMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+
+        }
+
+        public override string MenuText()
+        {
+            return "Select existing project";
+        }
+    }
+
+    #endregion
+
+    #region Project menu and items
+
+    class AddTransactionMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+            
+        }
+
+        public override string MenuText()
+        {
+            return "Add new transaction to project";
+        }
+    }
+
+    class RemoveTransactionMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+            
+        }
+
+        public override string MenuText()
+        {
+            return "Remove existing transaction from project";
+        }
+    }
+
+    class DisplayProjectSummaryMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+            
+        }
+
+        public override string MenuText()
+        {
+            return "Display a summary of this project's transactions";
+        }
+    }
+
+    class DisplayPurchaseMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+            
+        }
+
+        public override string MenuText()
+        {
+            return "Display purchases for this project";
+        }
+    }
+
+    class DisplaySalesMenu : ConsoleMenu
+    {
+        public override void CreateMenu()
+        {
+
+        }
+
+        public override string MenuText()
+        {
+            return "Display sales for this project";
+        }
+    }
+
+    #endregion
+}
